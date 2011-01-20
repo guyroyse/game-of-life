0afbfe2e2f2c3f1fe48363d7eb41ab1f794261bd
Guy Royse <guy@guyroyse.com>
2011-01-20 10:15:25 -0500

    Project skeleton

---
+++ test/GameOfLifeSpec.js
@@ -0,0 +1,20 @@
+describe("Jasmine Test Runner", function() {
+
+  it("Runs", function() {
+    expect(true).toBeTruthy();
+    expect(false).toBeFalsy();
+    expect(42).toEqual(42);
+  });
+
+  describe("Game of Life Tests", function() {
+
+    beforeEach(function() {
+      // any startup code goes here
+    });
+
+    it("..first test goes here..", function() {
+    });
+    
+  });
+
+});
