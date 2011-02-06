37ffc155e4335ce18a0786399d86f6d19429d91b
Guy Royse <guy@guyroyse.com>
2011-02-05 23:16:48 -0500

    Working on tests

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,4 +1,58 @@
-var GameBoard = function() {};
-GameBoard.prototype.setCell = function() {};
-GameBoard.prototype.pulse = function() {};
-GameBoard.prototype.getCell = function() {return false;};
+var GameBoard = function(width, height) {
+
+  var initializeBoard = function() {
+    var i, j, board = []
+    for (i = 0; i < width; i++) {
+      board[i] = [];
+      for (j = 0; j < height; j++)
+        board[i][j] = false;
+    }
+    return board;
+  };
+
+  var countNeighbors = function(x,y) {
+    var i, j, count = 0;
+    for (i = lowWidth(x); i <= highWidth(x); i++)
+      for (j = lowHeight(y); j <= highHeight(y); j++)
+        if (theBoard[i][j] == true) count++;
+    if (theBoard[x][y] == true) count--;
+  };
+
+  var lowWidth = function(x) {
+    return Math.max(x-1, 0);
+  };
+
+  var lowHeight = lowWidth;
+
+  var highWidth = function(x) {
+    return Math.min(x+1, width - 1);
+  };
+
+  var highHeight = function(x) {
+    return Math.min(x+1, height - 1);
+  };
+
+  var theBoard = initializeBoard();
+
+  this.setCellAlive = function(x,y) {
+    theBoard[x][y] = true;
+  };
+
+  this.getCellStatus = function(x,y) {
+    return theBoard[x][y];
+  };
+
+  this.pulse = function() {
+  
+    var i, j, newBoard = initializeBoard();
+    for (i = 0; i < width; i++) {
+      for (j = 0; j < height; j++) {
+        if (countNeighbors(i, j) == 2) newBoard[i][j] = true;
+      }
+    }
+
+    theBoard = newBoard;
+
+  };
+
+};
--- test/GameOfLifeSpec.js
+++ test/GameOfLifeSpec.js
@@ -10,15 +10,45 @@ describe("Jasmine Test Runner", function() {
 
     describe("Cell", function() {
 
+      var board;
+      var centerCell = function() {
+        return board.getCellStatus(1,1);
+      };
+
+      beforeEach(function() {
+        board = new GameBoard(3, 3);
+      });
+
       it("dies when it has no neighbors", function() {
-
-        var board = new GameBoard(1,1);
-        board.setCell(0, 0, true);
-
+        board.setCellAlive(1,1);
         board.pulse();
+        expect(centerCell()).toBeFalsy();
+      });
 
-        expect(board.getCell(0, 0)).toBeFalsy();
+      it("dies when it has four neighbors", function() {
+        board.setCellAlive(0,1);
+        board.setCellAlive(1,0);
+        board.setCellAlive(1,1);
+        board.setCellAlive(1,2);
+        board.setCellAlive(2,1);
+        board.pulse();
+        expect(centerCell()).toBeFalsy();
+      });
 
+      it("dies when it has eight neighbors", function() {
+        for(var i = 0; i < 3; i++)
+          for(var j = 0; j < 3; j++)
+            board.setCellAlive(i,j);
+        board.pulse();
+        expect(board.getCellStatus(1,1)).toBeFalsy();
+      });
+
+      it("lives when it has two neighbors", function() {
+        board.setCellAlive(1,0);
+        board.setCellAlive(1,1);
+        board.setCellAlive(1,2);
+        board.pulse();
+        expect(centerCell()).toBeTruthy();
       });
 
     });
