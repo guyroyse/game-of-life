abe42aa0132de77776f27fa18af9fb05596e92f3
Guy Royse <guy@guyroyse.com>
2011-02-09 22:29:01 -0500

    Ended up with a board object anyways

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,1 +1,28 @@
+var board = {
+	initialize : function() {},
+	status : function() {
+		return false;
+	}
+};
 
+function nextGeneration(board) {
+	
+	var error = "Argument is not a board";
+	if (board && board.isArray()) {
+		for (var i = 0; i < board.length; i++) {
+			if (board[i].isNotArray()) {
+				throw error;
+			}
+		}
+		return [];
+	}
+	throw error;
+}
+
+Object.prototype.isArray = function() {
+	return this instanceof Array;
+};
+
+Object.prototype.isNotArray = function() {
+	return !this.isArray();
+};
--- test/GameOfLifeSpec.js
+++ test/GameOfLifeSpec.js
@@ -6,13 +6,41 @@ describe("Jasmine Test Runner", function() {
     expect(42).toEqual(42);
   });
 
-  describe("Game of Life Tests", function() {
+  describe("Game of Life", function() {
 
     beforeEach(function() {
       // any startup code goes here
     });
 
-    it("..first test goes here..", function() {
+    it("returns a board when handed a board", function() {
+    	expect(nextGeneration([[]]) instanceof Array).toBeTruthy();
+    });
+    
+    it("throws an exception when not handed a board", function() {
+    	expect(nextGeneration).toThrow("Argument is not a board");
+    });
+    
+    it("throws an exception when handed a board that is an array", function() {
+    	expect(function() {
+    		nextGeneration({});
+    	}).toThrow("Argument is not a board");
+    });
+    
+    it("throws an excpetion when handed a board that is not a 2 dimensional array", function() {
+    	expect(function() {
+    		nextGeneration([{}]);
+    	}).toThrow("Argument is not a board");
+    });
+    
+    it("has a board", function() {
+    	expect(board).toBeDefined();
+    });
+    
+    describe("board", function() {
+    	it("it initializes with an empty board", function() {
+    		board.initialize(1, 1);
+    		expect(board.status(0, 0)).toBeFalsy();
+        });
     });
     
   });
