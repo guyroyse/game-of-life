4a5019256e4561387160ab5a1e023d16b3920158
Guy Royse <guy@guyroyse.com>
2011-02-18 04:35:58 -0500

    All tests pass... completed

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,30 +1,84 @@
 var board = (function() {
 	var cells;
-	return {
-		initialize : function(width, height) {
-			cells = [];
-			for(i = 0; i < width; i++) {
-				cells[i] = new Array(height);
+	var countNeighbors = function(row, col) {
+
+		var neighbors = 0;
+		var m, n;
+		
+		var thisIsMe = function() {
+			return (m == row) && (n == col);
+		};
+		
+		var thisIsOutOfBounds = function() {
+			return (m < 0) || (n < 0) || (m >= board.rows()) || (n >= board.columns()); 
+		};
+		
+		for (m = row - 1; m <= row + 1; m++) {
+			for (n = col - 1; n <= col + 1; n++) {
+				if(thisIsMe() || thisIsOutOfBounds()) {
+					continue;
+				}
+				
+				if(cells[m][n] == true) {
+					neighbors++;
+				}
 			}
+		}
+		
+		return neighbors;
+	}
+	var initializeBoard = function(rows, columns) {
+		var theBoard = [];
+		for(var i = 0; i < rows; i++) {
+			theBoard[i] = new Array(columns);
+		}
+		return theBoard;
+	}
+	return {
+		initialize : function(rows, columns) {
+			cells = initializeBoard(rows, columns);
 		},
-		width : function() {
+		rows : function() {
 			return cells.length;
 		},
-		height : function() {
+		columns : function() {
 			return cells[0].length;
 		},
-		status : function(x, y) {
-			return cells[x][y];
+		status : function(row, col) {
+			return cells[row][col];
 		},
-		makeAlive : function(x, y) {
-			cells[x][y] = true;
+		makeAlive : function(row, col) {
+			cells[row][col] = true;
 		},
-		makeDead : function(x, y) {
-			cells[x][y] = false;
+		makeDead : function(row, col) {
+			cells[row][col] = false;
 		}, 
-		nextGeneration : function() {
-			nextState = [];
+		printBoard : function() {
+			var s = "";
+			for (var row = 0; row < this.rows(); row++) {
+				for (var col = 0; col < this.columns(); col++) {
+					s+= this.status(row, col) ? "x" : "o"; 
+				}
+				s+= "\n";
+			}
 			
+			return s;
+		},
+		nextGeneration : function() {
+			nextState = initializeBoard(this.rows(), this.columns());
+
+			for (var row = 0; row < this.rows(); row++) {
+				for (var col = 0; col < this.columns(); col++) {
+					var neighbors = countNeighbors(row, col);
+					if (neighbors == 2){
+						nextState[row][col] = cells[row][col];
+					} else if(neighbors == 3) {
+						nextState[row][col] = true;
+					}
+					
+				}
+			}
+			cells = nextState;
 		}
 	};
 })();
--- test/GameOfLifeSpec.js
+++ test/GameOfLifeSpec.js
@@ -39,18 +39,91 @@ describe("Jasmine Test Runner", function() {
     	
     	it("has a height and width", function(){
     		board.initialize(2, 3);
-    		expect(board.width()).toEqual(2);
-    		expect(board.height()).toEqual(3);
+    		expect(board.rows()).toEqual(2);
+    		expect(board.columns()).toEqual(3);
     	});
     	
     	describe("Cell", function() {
         
+        	beforeEach(function() {
+        		board.initialize(3,3);
+        	})
+        	
     		it("dies when it has no neighbors", function() {
-    			board.initialize(3, 3);
         		board.makeAlive(1, 1);
         		board.nextGeneration();
         		expect(board.status(1, 1)).toBeFalsy();
         	});
+    		
+    		it("dies when it has one neighbor", function() {
+    			board.makeAlive(1, 1);
+    			board.makeAlive(1, 2);
+    			board.nextGeneration();
+    			expect(board.status(1, 1)).toBeFalsy();
+    		});
+
+    		it("stays alive when it has two neighbors", function() {
+    			board.makeAlive(1, 0);
+    			board.makeAlive(1, 1);
+    			board.makeAlive(1, 2);
+    			board.nextGeneration();
+    			expect(board.status(1, 1)).toBeTruthy();
+    		});
+    		
+    		it("stays dead when it has two neighbors", function() {
+    			board.makeAlive(1, 0);
+    			board.makeAlive(1, 2);
+    			board.nextGeneration();
+    			expect(board.status(1, 1)).toBeFalsy();
+    		});
+    		
+    		it("stays alive when it has three neighbors", function() {
+    			board.makeAlive(1, 0);
+    			board.makeAlive(1, 1);
+    			board.makeAlive(1, 2);
+    			board.makeAlive(2, 1);
+    			board.nextGeneration();
+    			expect(board.status(1, 1)).toBeTruthy();
+    		});
+    		
+    		it("comes alive when it has three neighbors", function() {
+    			board.makeAlive(1, 0);
+    			board.makeAlive(1, 2);
+    			board.makeAlive(2, 1);
+    			board.nextGeneration();
+    			expect(board.status(1, 1)).toBeTruthy();
+    		});
+    		
+    		it("dies when it has more than three neighbors", function() {
+    			board.makeAlive(0, 1);
+    			board.makeAlive(1, 0);
+    			board.makeAlive(1, 1);
+    			board.makeAlive(1, 2);
+    			board.makeAlive(2, 1);
+    			board.nextGeneration();
+    			expect(board.status(1, 1)).toBeFalsy();
+    		});
+    		
+    		it("stays dead when it has more than three neighbors", function() {
+    			board.makeAlive(0, 1);
+    			board.makeAlive(1, 0);
+    			board.makeAlive(1, 2);
+    			board.makeAlive(2, 1);
+    			board.nextGeneration();
+    			expect(board.status(1, 1)).toBeFalsy();
+    		});
+    		
+    		it("pulses", function() {
+    			board.makeAlive(1, 0);
+    			board.makeAlive(1, 1);
+    			board.makeAlive(1, 2);
+    			expect(board.printBoard()).toEqual("ooo\nxxx\nooo\n");
+    			board.nextGeneration();
+    			expect(board.printBoard()).toEqual("oxo\noxo\noxo\n");
+    			board.nextGeneration();
+    			expect(board.printBoard()).toEqual("ooo\nxxx\nooo\n");
+    		});
+    		
 
     	});
     	
